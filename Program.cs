29ac5411c10387d17bb66181a02503ecaf938b99
aaronjoshua27 <73965654+aaronpangan@users.noreply.github.com>
2021-11-12 22:54:08 +0800

    Initialize Logger

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Serilog;
+using Serilog.Events;
 
 namespace Hotel_API
 {
@@ -13,11 +15,32 @@ namespace Hotel_API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //Initialize Logger
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.File(path: "C:\\Users\\AaronJoshua\\Desktop\\.NET\\Hotel-API\\logs\\log-.txt",
+                    outputTemplate:
+                    "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}",
+                    rollingInterval: RollingInterval.Day,
+                    restrictedToMinimumLevel: LogEventLevel.Information
+                ).CreateLogger();
+            try
+            {
+                Log.Information("Application Started");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception e)
+            {
+                Log.Fatal(e, "Application Failed to Start");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
--- Startup.cs
+++ Startup.cs
@@ -37,6 +37,7 @@ namespace Hotel_API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -50,6 +51,7 @@ namespace Hotel_API
 
             app.UseAuthorization();
 
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
@@ -57,3 +59,5 @@ namespace Hotel_API
         }
     }
 }
+
+
