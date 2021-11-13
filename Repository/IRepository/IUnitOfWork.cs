7d202c62c8f94385ca5998f3f874cabc5f526485
aaronjoshua27 <73965654+aaronpangan@users.noreply.github.com>
2021-11-14 04:12:14 +0800

    Implement Data Repository

---
+++ Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hotel_API.IRepository;
+using Hotel_API.Model;
+
+namespace Hotel_API.Repository.IRepository
+{
+    internal interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<CountryModel> Countries { get; }
+        IGenericRepository<HotelModel> Hotels { get; }
+
+        Task Save();
+    }
+}
---
+++ Repository/UnitOfWork.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hotel_API.IRepository;
+using Hotel_API.Model;
+using Hotel_API.Repository.IRepository;
+
+namespace Hotel_API.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DatabaseContext _context;
+
+        private IGenericRepository<CountryModel> _countries;
+        private IGenericRepository<HotelModel> _hotels;
+
+        public UnitOfWork(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public IGenericRepository<CountryModel> Countries => _countries ??= new GenericRepository<CountryModel>(_context);
+
+        public IGenericRepository<HotelModel> Hotels => _hotels ??= new GenericRepository<HotelModel>(_context);
+
+        public void Dispose()
+        {
+            _context.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
