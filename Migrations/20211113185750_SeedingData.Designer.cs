a2012536183c1e973aa38577961399e0c6b0f1a4
aaronjoshua27 <73965654+aaronpangan@users.noreply.github.com>
2021-11-14 02:58:42 +0800

    Add Data Seeding

--- Migrations/20211113185750_SeedingData.Designer.cs
+++ Migrations/20211113185750_SeedingData.Designer.cs
@@ -3,14 +3,16 @@ using Hotel_API.Model;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Hotel_API.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20211113185750_SeedingData")]
+    partial class SeedingData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Model/DatabaseContext.cs
+++ Model/DatabaseContext.cs
@@ -14,5 +14,51 @@ namespace Hotel_API.Model
         public DbSet<CountryModel> Country { get; set; }
 
         public DbSet<HotelModel> Hotel { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<CountryModel>().HasData(
+                new CountryModel
+                {
+                    Id = 1,
+                    Name = "Jamaica",
+                    ShortName = "JM"
+                }, new CountryModel
+                {
+                    Id = 2,
+                    Name = "Bahamas",
+                    ShortName = "BS"
+                }, new CountryModel
+                {
+                    Id = 3,
+                    Name = "Cayman Island",
+                    ShortName = "CI"
+                }
+            );
+            builder.Entity<HotelModel>().HasData(
+                new HotelModel
+                {
+                    Id = 1,
+                    Name = "Sandals Resort and Spa",
+                    Address = "Negril",
+                    CountryId = 1,
+                    Rating = 4.5
+                }, new HotelModel
+                {
+                    Id = 2,
+                    Name = "Comfort Suites",
+                    Address = "George Town",
+                    CountryId = 3,
+                    Rating = 4.3
+                }, new HotelModel
+                {
+                    Id = 3,
+                    Name = "Grand Palldium",
+                    Address = "Nassua",
+                    CountryId = 2,
+                    Rating = 4
+                }
+            );
+        }
     }
 }
